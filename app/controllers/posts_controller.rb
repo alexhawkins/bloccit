bc4b2b374bf6c04c2f20cebda645135e22a464fc
Alex Hawkins <alexhawkins.me@gmail.com>
2014-03-30 20:58:11 +0000

    Add create and update Crud actions with basic styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    @comments = Comment.all
   end
 
   def show
@@ -12,6 +11,29 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post  = Post.new(params.require(:post).permit(:title, :body))
+      if @post.save
+        flash[:notice] = "Post was saved"
+        redirect_to @post
+      else
+        flash[:notice] = "There was an error saving the post. Please try again."
+        render :new
+      end
+  end
+
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params.require(:post).permit(:title, :body))
+      flash[:notice] = "Post was updated"
+      redirect_to @post
+    else
+      flash[:notice] = "There was an error saving the post. Please try again."
+      render :edit
+    end
   end
 end
