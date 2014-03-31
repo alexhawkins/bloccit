8a4e98c50758d106202a3a2a770c0b56ed95843e
Alex Hawkins <alexhawkins.me@gmail.com>
2014-03-31 20:36:25 +0000

    Associate users and posts, Add custom styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   #the has_many method tells Rails that a Post can havy many
   #comments related to is
   has_many :comments
+  belongs_to :user
+  #Order the post by their created_at date, in descending order
+  # displays the most recent posts first
+  default_scope { order('created_at DESC') }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,36 @@
 
 require 'faker'
 
-24.times do
-  p = Post.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
-  3.times do
-    p.comments.create(body: Faker::Lorem.paragraph)
-  end  
+# Create 5 users with their own posts
+5.times do
+  password = Faker::Lorem.characters(10)
+  user = User.new(
+    name: Faker::Name.name, 
+    email: Faker::Internet.email, 
+    password: password, 
+    password_confirmation: password)
+  user.skip_confirmation!
+  user.save
+
+  # Note: by calling `User.new` instead of `create`,
+  # we create an instance of a user which isn't saved to the database.
+  # The `skip_confirmation!` method sets the confirmation date
+  # to avoid sending an email. The `save` method updates the database.
+
+  5.times do
+    post = Post.create(
+      user: user,
+      title: Faker::Lorem.sentence, 
+      body: Faker::Lorem.paragraph)
+    # set the created_at to a time within the past year
+    post.update_attribute(:created_at, Time.now - rand(600..31536000))
+  end
 end
 
+user = User.first
+user.skip_confirmation!
+user.update_attributes(email: 'youremail.com', password: 'helloworld', password_confirmation: 'helloworld')
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
