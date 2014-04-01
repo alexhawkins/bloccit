8678e7a3032711df680487fe5f75a58aebb826d3
Alex Hawkins <alexhawkins.me@gmail.com>
2014-04-01 15:38:36 -0700

    Refactor HTML, Add Helper Methods for breadcrumbs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  #Set the full title here that you will use for Breadcrumbs. 
+  #That way, if we change the home page name, we
+  #only have to change it once.
+  def home_title
+    home_title = "Bloccit"
+    home_title
+  end
 end
