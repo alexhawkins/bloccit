ff1b46cdebf92449d8b82c034428ca7865454513
Alex Hawkins <alexhawkins.me@gmail.com>
2014-04-02 16:41:06 -0700

    Install Red Carpet, Add related post logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,10 @@ module ApplicationHelper
     end
   end
 
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render text).html_safe
+  end
 end
